979508d01e62dbf6f1916cf2b0da93f6f83d757a
source <mom.sombrathna26@gmail.com>
2023-10-11 15:08:49 +0700

    push

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -107,9 +107,9 @@ import { Link } from '@mui/material';
   
             <Grid container spacing={4}>
               <Grid item xs={4}>
-                  <div className="container">
+                  <div className="container" onClick={() => selectDropdownItem('design-dropdown', 'T-shirt')} style={{cursor:'pointer'}}>
                     <img width="96" height="96" src="https://img.icons8.com/fluency/96/polo-shirt.png" alt="polo-shirt"  
-                     onClick={() => selectDropdownItem('design-dropdown', 'T-shirt')} style={{cursor:'pointer'}}/>
+                     />
                     <div class="overlay">
                       <div class="text name">T-shirt</div>
                     </div>
@@ -125,36 +125,36 @@ import { Link } from '@mui/material';
                   </div>
               </Grid>
               <Grid item xs={4}>
-                  <div class="container">
+                  <div class="container" onClick={() => selectDropdownItem('design-dropdown', 'Jeans')} style={{cursor:'pointer'}}>
                     <img width="100" height="100" src="https://img.icons8.com/plasticine/100/jeans.png" alt="jeans"
-                     onClick={() => selectDropdownItem('design-dropdown', 'Jeans')} style={{cursor:'pointer'}}/> 
+                     /> 
                      <div class="overlay">
                       <div class="text name">Jeans</div>
                     </div> 
                   </div>
               </Grid>
               <Grid item xs={4}>
-                <div class="container">
+                <div class="container" onClick={() => selectDropdownItem('design-dropdown', 'Dress')} style={{cursor:'pointer'}}>
                   <img width="96" height="96" src="https://img.icons8.com/emoji/96/dress.png" alt="dress"
-                     onClick={() => selectDropdownItem('design-dropdown', 'Dress')} style={{cursor:'pointer'}}/>
+                     />
                      <div class="overlay">
                       <div class="text name">Dress</div>
                     </div>
                 </div>
               </Grid>
               <Grid item xs={4}>
-                <div class="container">
+                <div class="container" onClick={() => selectDropdownItem('design-dropdown', 'Demin Shorts')} style={{cursor:'pointer'}}>
                 <img width="96" height="96" src="https://img.icons8.com/color/96/denim-shorts.png" alt="denim-shorts"
-                     onClick={() => selectDropdownItem('design-dropdown', 'Demin Shorts')} style={{cursor:'pointer'}}/>
+                     />
                      <div class="overlay">
                       <div class="text name">Denim Shorts</div>
                     </div>
                 </div>
               </Grid>
               <Grid item xs={4}>
-                <div class="container">
+                <div class="container" onClick={() => selectDropdownItem('design-dropdown', 'Suit')} style={{cursor:'pointer'}}>
                 <img width="100" height="100" src="https://img.icons8.com/plasticine/100/suit.png" alt="suit"
-                     onClick={() => selectDropdownItem('design-dropdown', 'Suit')} style={{cursor:'pointer'}}/>
+                     />
                      <div class="overlay">
                       <div class="text name">Suit</div>
                     </div>
